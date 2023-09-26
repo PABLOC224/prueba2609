585466d3ff33372992d57c1569ecd81336bc2a44
Pablo Canseco <pablocanseco2014@gmail.com>
2023-09-26 11:43:45 -0300

    Añadido archivo de configuración

---
+++ prueba.js
@@ -0,0 +1,14 @@
+// Declarar una función
+function saludar(nombre) {
+    console.log(`¡Hola, ${nombre}!`);
+  }
+  
+  const numeros = [1, 2, 3, 4, 5];
+
+numeros.forEach(function(numero) {
+  saludar("pablo" + numero);
+});
+
+  
+
+
